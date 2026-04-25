5484dd90458de595ee754da0e31d605dd94b53b0
Daniel <danieldfcreis@gmail.com>
2026-04-25 00:14:15 -0300

    feat: sets up the API to start the project in SPRINGBOOT

---
+++ src/main/java/com/movieDesk/MovieDesk/MovieDeskApplication.java
@@ -0,0 +1,13 @@
+package com.movieDesk.MovieDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movieDesk/MovieDesk/MovieDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieDesk.MovieDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
