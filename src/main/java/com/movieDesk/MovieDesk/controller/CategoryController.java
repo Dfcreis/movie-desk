1791d00aedb70883af94343d0c0e12352af3e928
Daniel <danieldfcreis@gmail.com>
2026-04-26 12:44:27 -0300

    feat: add endpoints for listAll, listById and deleteById

--- src/main/java/com/movieDesk/MovieDesk/controller/CategoryController.java
+++ src/main/java/com/movieDesk/MovieDesk/controller/CategoryController.java
@@ -3,12 +3,10 @@ package com.movieDesk.MovieDesk.controller;
 import com.movieDesk.MovieDesk.entity.Category;
 import com.movieDesk.MovieDesk.services.CategoryServices;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movieDesk/category")
@@ -24,9 +22,26 @@ public class CategoryController {
     }
 
     @PostMapping("/create")
-    public Category createCategory(Category category){
+    public Category createCategory(@RequestBody Category category){
         return categoryServices.createCategory(category);
     }
+
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable Long id){
+        Optional<Category>optionalCategory = categoryServices.getCategoryById(id);
+        if (optionalCategory.isPresent()){
+            return optionalCategory.get();
+        }
+        return null;
+    }
+
+
+    @DeleteMapping("delete/{id}")
+    public void deleteCategoryById(@PathVariable Long id){
+        categoryServices.deleteCategoryById(id);
+    }
+
+
 
 
 
